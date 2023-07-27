d724e403f6d61b575c10745c1ad6b1d0891d9875
christoph.raddatz <hello@chrstphrddtz.com>
2023-07-27 14:59:45 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/navigation";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={""} />
+      </Route>
     </Routes>
   );
 }
