374f0d01889c6d2612ebeac4156b00cb219b0ec7
christoph.raddatz <hello@chrstphrddtz.com>
2023-07-27 16:05:39 +0200

    add routes for shop and contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home";
 import Navigation from "./routes/navigation/navigation";
+import Shop from "./routes/shop/shop";
+import Contact from "./routes/contact/contact";
 import SignIn from "./routes/sign-in/sign-in";
 
 export default function App() {
@@ -8,8 +10,8 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={""} />
-        <Route path="contact" element={""} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="contact" element={<Contact />} />
         <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
---
+++ src/routes/shop/shop.jsx
@@ -0,0 +1,7 @@
+export default function Shop() {
+  return (
+    <div>
+      <h1>shop side</h1>
+    </div>
+  );
+}
