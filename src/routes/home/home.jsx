d92eed1900845e8b04d315bb5900f4b7f5fcf5dd
christoph.raddatz <hello@chrstphrddtz.com>
2023-07-27 14:59:18 +0200

    adjust imports

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,5 +1,6 @@
-import Directory from "../../components/directory/directory";
+
 import categories from "../../resources/categories.json";
+import Directory from "../../components/directory/directory";
 
 export default function Home() {
   return <Directory categories={categories} />;
