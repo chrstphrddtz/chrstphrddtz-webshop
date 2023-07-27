b0e43b69d6ff1137d117f7c84d4950ab5a5b09ee
christoph.raddatz <hello@chrstphrddtz.com>
2023-07-27 13:33:00 +0200

    refactor App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
-import "./App.css";
+// import "./categories.styles.scss";
+
+import CategoryMenu from "./components/categopry-item/category-menu/category-menu";
 
 export default function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Womens",
-    },
-    {
-      id: 5,
-      title: "Mens",
-    },
-  ];
-
-  return (
-    <div className="category-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
-          <div className="background-image"/>
-          {/* <img/> */}
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <CategoryMenu />;
 }
