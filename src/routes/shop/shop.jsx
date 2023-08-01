759fe977e2ff2a3ce00f9a6f97cb8500b4de488f
christoph.raddatz <hello@chrstphrddtz.com>
2023-08-01 13:42:21 +0200

    add shop page & layout

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { ProductsContext } from "../../contexts/products-context";
+
+import ProductCard from "../../components/product-card/product-card";
+import "./shop.styles.scss"
+
 export default function Shop() {
+  const { products } = useContext(ProductsContext);
+  console.log(products);
+
   return (
-    <div>
-      <h1>shop side</h1>
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   );
 }
