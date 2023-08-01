b05325056df55e65ed1444ee597ea1f51b78ed43
christoph.raddatz <hello@chrstphrddtz.com>
2023-08-01 15:06:36 +0200

    add context+cardIcon component to navigation

--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -1,15 +1,20 @@
 import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import { UserContext } from "../../contexts/user-context";
-
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { UserContext } from "../../contexts/user-context";
+import { CartContext } from "../../contexts/cart-context";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import CartIcon from "../../components/cart-icon/cart-icon";
+import CartDropDown from "../../components/cart-dropdown/cart-dropdown.jsx";
+
 import "./navigation.styles.scss";
 
 export default function Navigation() {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <>
@@ -36,7 +41,9 @@ export default function Navigation() {
               sign in
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropDown />}
       </nav>
       <Outlet />
     </>
